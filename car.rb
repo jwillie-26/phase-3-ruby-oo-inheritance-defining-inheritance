230c5d2700d0d5a1b116526afef0d3f3b7baf7ed
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2023-02-13 15:52:25 +0300

    Delete car.rb

--- car.rb
+++
@@ -1,8 +0,0 @@
-# This loads the code from the vehicle.rb file so we can access its code within this file
-require_relative "./vehicle"
-
-class Car < Vehicle
-  def go
-    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-  end
-end
